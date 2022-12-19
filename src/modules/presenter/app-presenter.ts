00fc17fa5fe1a4e81088e57b2768d9bab278b578
Natalya <m_d.r.lecter@protonmail.com>
2022-12-20 03:15:57 +0400

    table rows and partly buttons

--- src/modules/presenter/app-presenter.ts
+++ src/modules/presenter/app-presenter.ts
@@ -1,8 +1,7 @@
-import { nanoid } from "../../../node_modules/nanoid/index";
 import { TaskInterface } from "../create-task";
-import { addTask, changeTask, getTaskList, removeTask } from "../model/task-model";
+import { getTaskList } from "../model/task-model";
 import { createAppMarkUp } from "../view/app-view";
-import { createTasksMarkUp } from "./task-presenter";
+import { completeButtonHandler, createTasksMarkUp, removeButtonHandler, saveButtonHandler } from "./task-presenter";
 
 export const renderApp = () => {
     const body = document.querySelector('body') as HTMLBodyElement;
@@ -11,50 +10,17 @@ export const renderApp = () => {
     renderTasksList(getTaskList());
     saveButtonHandler();
     removeButtonHandler();
+    completeButtonHandler();
 }
 
 export const renderTasksList = (taskList: TaskInterface[]) => {
     const tBody = document.querySelector('tbody') as HTMLTableSectionElement;
     tBody?.insertAdjacentHTML('beforeend', createTasksMarkUp(taskList));
-    //TODO функция перерисовки всех номеров
-}
 
-export const removeTableString = (cell: HTMLElement): void => {
-    cell.parentElement?.remove();
-}
+    const table = document.querySelector('.table') as HTMLTableElement;
+    const rows: HTMLCollectionOf<HTMLTableRowElement> = table.rows;
 
-const saveButtonHandler = () => {
-    const buttonSave = document.querySelector('.btn-primary') as HTMLButtonElement;
-
-    buttonSave.addEventListener('click', (e) => {
-        e.preventDefault();
-
-        const input = document.querySelector('.form-control') as HTMLInputElement;
-        const newTask: TaskInterface = {
-            id: nanoid(),
-            taskText: input.value,
-            status: 'running',
-        }
-
-        changeTask(addTask, newTask);
-        renderTasksList([newTask]);
-    })
-}
-
-const removeButtonHandler = () => {
-    const buttonsRemove = document.querySelectorAll('.btn-danger') as NodeListOf<HTMLButtonElement>;
-    buttonsRemove.forEach(buttonRemove =>
-        buttonRemove.addEventListener('click', (e) => {
-            e.preventDefault();
-            const target = e.target as HTMLButtonElement;
-
-            if (target.parentElement) {
-                const removedTask: TaskInterface = {
-                    id: target.parentElement.id,
-                }
-                changeTask(removeTask, removedTask);
-                removeTableString(target.parentElement);
-            }
-        })
-    );
+    for (let i = 1; i < rows.length; i++) {
+        rows[i].children.item(0)!.textContent = String(i);
+    }
 }
