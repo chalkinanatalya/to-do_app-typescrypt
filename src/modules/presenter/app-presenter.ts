866f879a0b3b91b182ca3fa4952583a7f540c672
Natalya <m_d.r.lecter@protonmail.com>
2022-12-21 01:06:02 +0400

    clear

--- src/modules/presenter/app-presenter.ts
+++ src/modules/presenter/app-presenter.ts
@@ -5,6 +5,8 @@ import { completeButtonHandler, createTasksMarkUp, removeButtonHandler, saveButt
 
 export const renderApp = () => {
     const body = document.querySelector('body') as HTMLBodyElement;
+
+
     body?.insertAdjacentHTML('beforeend', createAppMarkUp());
 
     renderTasksList(getTaskList());
@@ -17,6 +19,13 @@ export const renderTasksList = (taskList: TaskInterface[]) => {
     const tBody = document.querySelector('tbody') as HTMLTableSectionElement;
     tBody?.insertAdjacentHTML('beforeend', createTasksMarkUp(taskList));
 
+    const buttons = document.querySelectorAll('.btn') as NodeListOf<HTMLButtonElement>;
+    pageHandler(buttons);
+
+    Array.from(buttons).forEach(button => {
+        button.disabled = true;
+    });
+
     const table = document.querySelector('.table') as HTMLTableElement;
     const rows: HTMLCollectionOf<HTMLTableRowElement> = table.rows;
 
@@ -27,7 +36,30 @@ export const renderTasksList = (taskList: TaskInterface[]) => {
 
 const evokeHandler = () => {
     const buttonSave = document.querySelector('.btn-primary') as HTMLButtonElement;
+    const buttonClear = document.querySelector('.btn-warning') as HTMLButtonElement;
     const input = document.querySelector('.form-control') as HTMLInputElement;
     saveButtonHandler(buttonSave, input);
+    clearButtonHandler(buttonClear, buttonSave, input);
     stateHandler(buttonSave, input);
 }
+
+export const pageHandler = (buttons: NodeListOf<HTMLButtonElement>) => {
+    window.addEventListener('load', () => {
+        Array.from(buttons).forEach(button => {
+            button.disabled = false;
+            evokeHandler();
+        });
+    });
+
+    //TODO check if it sets disabled
+}
+
+export const clearButtonHandler = (buttonClear: HTMLButtonElement, buttonSave: HTMLButtonElement, input: HTMLInputElement) => {
+    buttonClear.addEventListener('click', (e) => {
+        e.preventDefault();
+        input.value = '';
+
+        buttonSave.disabled = true;
+    })
+}
+
